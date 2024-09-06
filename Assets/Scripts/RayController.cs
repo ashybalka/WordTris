088173ff30fae6a2ff89d8f6a1b73d5b24b83b52
Artyom <artemshibalko@gmail.com>
2024-09-06 14:31:41 +0300

    Prepare for char array

--- Assets/Scripts/RayController.cs
+++ Assets/Scripts/RayController.cs
@@ -2,17 +2,42 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
 
 public class RayController : MonoBehaviour
 {
     public List<string> currentGameWords = new();
+    public char[,] gameField = new char[9,9];
 
-    [SerializeField] GameObject Raycasters;
+    [SerializeField] GameObject Raycasters, Letters;
 
     public void GetRaysToLetters()
     {
         currentGameWords.Clear();
+
+        {
+            for (int i = 0; i < gameField.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameField.GetLength(1); j++)
+                {
+                    gameField[i, j] = ' ';
+                }
+            }
+        }
+
+        foreach (var letterFigure in Letters.GetComponentsInChildren<BoxCollider2D>())
+        {
+            gameField[(int)(letterFigure.transform.position.x + 4f), (int)(4.5f - letterFigure.transform.position.y)] = 
+                letterFigure.GetComponent<FigureController>().figureChar;
+        }
+
+        foreach (char ch in gameField)
+        { 
+            Debug.Log($"{ch}");
+        }
+
+        // Get Rays To Each Line
         foreach (Transform rays in Raycasters.GetComponentsInChildren<Transform>())
         {
             string tempStr = RayToLetters(rays.gameObject);
